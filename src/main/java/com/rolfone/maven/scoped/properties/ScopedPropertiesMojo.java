59850c37bbd5f873d05639797509411703ae3d46
famaridon <florent.amaridon@gmail.com>
2014-07-09 22:44:44 +0200

    1st commit

---
+++ src/main/java/com/rolfone/maven/scoped/properties/ScopedPropertiesMojo.java
@@ -0,0 +1,127 @@
+package com.rolfone.maven.scoped.properties;
+
+import com.rolfone.maven.scoped.properties.beans.Wrapper;
+import com.rolfone.maven.scoped.properties.beans.properties.Property;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by famaridon on 19/05/2014.
+ *
+ * @author famaridon
+ */
+@Mojo(name = "build-properties-files", threadSafe = false, requiresProject = false, requiresDirectInvocation = true)
+public class ScopedPropertiesMojo extends AbstractMojo
+{
+
+	/**
+	 * specify the input XML descriptor folder
+	 */
+	@Parameter(property = "propertiesXmlFolder", required = true)
+	protected File propertiesXmlFolder;
+
+	/**
+	 * specify the .properties file output folder
+	 */
+	@Parameter(property = "outputFolder", required = true)
+	protected File outputFolder;
+
+	/**
+	 * the XML descriptor's scope used to build .properties files
+	 */
+	@Parameter(property = "targetScope", required = true)
+	protected String targetScope;
+
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException
+	{
+
+		JAXBContext jaxbContext = null;
+		Unmarshaller unmarshaller = null;
+		try
+		{
+			jaxbContext = JAXBContext.newInstance(Wrapper.class, Property.class);
+			unmarshaller = jaxbContext.createUnmarshaller();
+		} catch (JAXBException e)
+		{
+			throw new MojoExecutionException(e.getMessage());
+		}
+
+
+		File[] propertiesXmlFiles = propertiesXmlFolder.listFiles((FileFilter) new SuffixFileFilter(".properties.xml"));
+		for (File proprtiesXml : propertiesXmlFiles)
+		{
+			try
+			{
+				Wrapper<Property> wrapper = (Wrapper<Property>) unmarshaller.unmarshal(proprtiesXml);
+
+				Properties properties = new Properties();
+				for (Property property : wrapper.getItems())
+				{
+					properties.setProperty(property.getName(), property.getValues().get(targetScope));
+				}
+
+				try (FileWriter writer = new FileWriter(new File(outputFolder, FilenameUtils.getBaseName(proprtiesXml.getName()))))
+				{
+					properties.store(writer, "Maven plugin building file for scope : " + targetScope);
+				} catch (IOException e)
+				{
+					throw new MojoFailureException("can't write properties file", e);
+				}
+
+
+			} catch (JAXBException e)
+			{
+				throw new MojoFailureException("can't read xml file : " + proprtiesXml.getAbsolutePath(), e);
+			}
+
+		}
+
+
+	}
+
+	public File getPropertiesXmlFolder()
+	{
+		return propertiesXmlFolder;
+	}
+
+	public void setPropertiesXmlFolder(File propertiesXmlFolder)
+	{
+		this.propertiesXmlFolder = propertiesXmlFolder;
+	}
+
+	public File getOutputFolder()
+	{
+		return outputFolder;
+	}
+
+	public void setOutputFolder(File outputFolder)
+	{
+		this.outputFolder = outputFolder;
+	}
+
+	public String getTargetScope()
+	{
+		return targetScope;
+	}
+
+	public void setTargetScope(String targetScope)
+	{
+		this.targetScope = targetScope;
+	}
+}
---
+++ src/main/java/com/rolfone/maven/scoped/properties/beans/properties/Property.java
@@ -0,0 +1,44 @@
+package com.rolfone.maven.scoped.properties.beans.properties;
+
+import com.rolfone.maven.scoped.properties.beans.adapter.EnvironmentAdapter;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * this is simple property bean
+ * Created by famaridon on 07/07/2014.
+ *
+ * @author famaridon
+ */
+@XmlRootElement
+public class Property
+{
+	private String name;
+	private Map<String, String> values = new HashMap<>();
+
+	@XmlAttribute
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	@XmlJavaTypeAdapter(EnvironmentAdapter.class)
+	public Map<String, String> getValues()
+	{
+		return values;
+	}
+
+	public void setValues(Map<String, String> values)
+	{
+		this.values = values;
+	}
+}
